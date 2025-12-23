6b467442a155a74861b4554878b1ca6803b37b80
zRanqa <jonnoach@gmail.com>
2025-12-23 21:33:48 +1100

    Added create account functionality with code

--- Poker Tracker/Model/PostRequest.swift
+++ Poker Tracker/Model/PostRequest.swift
@@ -7,7 +7,29 @@
 
 import Foundation
 
-func postRequest(to urlString: String, jsonBody: [String: Any], completion: @escaping (Result<String, Error>) -> Void) {
+enum API {
+    static let baseURL = "http://127.0.0.1:5000/api/"
+
+    case createVerificationCode
+    case createAccount
+
+    var url: String {
+        switch self {
+        case .createVerificationCode: return API.baseURL + "create-verification-code"
+        case .createAccount: return API.baseURL + "create-account"
+        }
+    }
+}
+
+
+struct APIResponse: Codable {
+    let status: String
+    let message: String
+    let data: String?
+}
+
+func postRequest(to urlString: String, jsonBody: [String: Any], completion: @escaping (Result<APIResponse, Error>) -> Void) {
+    
     guard let url = URL(string: urlString) else {
         completion(.failure(NSError(domain: "Invalid URL", code: 0)))
         return
@@ -30,11 +52,16 @@ func postRequest(to urlString: String, jsonBody: [String: Any], completion: @esc
             return
         }
 
-        guard let data = data, let responseString = String(data: data, encoding: .utf8) else {
-            completion(.failure(NSError(domain: "Invalid response", code: 0)))
+        guard let data = data else {
+            completion(.failure(NSError(domain: "No data", code: 0)))
             return
         }
-
-        completion(.success(responseString))
+        print(String(data: data, encoding: .utf8) ?? "invalid json")
+        do {
+            let apiResponse = try JSONDecoder().decode(APIResponse.self, from: data)
+            completion(.success(apiResponse))
+        } catch {
+            completion(.failure(error))
+        }
     }.resume()
 }
