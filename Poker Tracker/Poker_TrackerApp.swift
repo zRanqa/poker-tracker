623cd4051c147d3f061a59485b81eddf57808861
zRanqa <jonnoach@gmail.com>
2026-01-27 13:40:26 +1100

    Initial Commit

---
+++ Poker Tracker/Poker_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poker_TrackerApp.swift
+//  Poker Tracker
+//
+//  Created by Jonathan Dummett on 27/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct Poker_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Poker TrackerTests/Poker_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Poker_TrackerTests.swift
+//  Poker TrackerTests
+//
+//  Created by Jonathan Dummett on 27/1/2026.
+//
+
+import Testing
+@testable import Poker_Tracker
+
+struct Poker_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
